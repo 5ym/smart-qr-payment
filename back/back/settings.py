7831ace5e637b07e2d434878baab0ad624ac2831
5ym <16yuim@gmail.com>
2020-09-08 10:58:44 +0900

    開発活況整備

--- back/back/settings.py
+++ back/back/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sqp',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST': 'sqp-db',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",},
     }
 }
 
